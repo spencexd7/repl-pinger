5fd346f4de6fdf2d1a04655fb8c6585456c5cede
spencexd7 <131441794+spencexd7@users.noreply.github.com>
2023-05-03 18:27:50 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,26 @@
+# config
+
+web_url = "url"
+
+# import
+
+import httpx 
+
+# code
+
+def start_pinging(web):
+    try:
+        httpx.get(f"http://{web}")
+        return True
+    except:
+        return False
+
+while True:
+        req = start_pinging(web_url)
+        if req == True:
+            print("[+] up\n\n")
+        else:
+            print("[-] down\n\n")
+
+
+# @ spencexd7
